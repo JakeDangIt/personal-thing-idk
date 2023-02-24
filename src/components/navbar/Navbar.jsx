24f8700d09b279f002d7c8c835d8c7dc87002719
JakeDangIt <diam0ndjake07@gmail.com>
2023-02-24 03:30:31 -0500

    complete navbar
    media queries too

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,12 @@
 import './navbar.css';
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri"
 import logo from '../../assets/celeste-logo.png'
-import React from 'react';
+import React, {useState} from 'react';
+
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className='celeste__navbar'>
       <div className='celeste__navbar-links'>
@@ -21,6 +24,27 @@ const Navbar = () => {
       <div className='celeste__navbar-sign'>
         <p>Sign in</p>
         <button type='button'>Sign up</button>
+      </div>
+      <div className='celeste__navbar-menu'>
+        {toggleMenu
+          ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)}/>
+          : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)}/>
+        }
+        {toggleMenu && (
+          <div className='celeste__navbar-menu_container scale-up-center'>
+            <div className='celeste__navbar-menu_container-links'>
+              <p><a href='#home'>Home</a></p>
+              <p><a href='#gameplay'>Gameplay</a></p>
+              <p><a href='#mechanics'>Mechanics</a></p>
+              <p><a href='#speedrunning'>Speedrunning</a></p>
+              <p><a href='#purchase'>Purchase</a></p>
+            </div>
+            <div className='celeste__navbar-menu_container-link-sign'>
+              <p>Sign in</p>
+              <button type='button'>Sign up</button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
