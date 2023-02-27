d0edcca3553acdab01bfdf3db0d0bc949b9c116c
JakeDangIt <diam0ndjake07@gmail.com>
2023-02-26 21:55:09 -0500

    brand colors, transitions, and links

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -4,21 +4,31 @@ import {steam, xbox, ps, epic, nintendo} from './imports.js'
 
 const Brand = () => {
   return (
-    <div className='celeste__brand section__padding'>
+    <div className='celeste__brand section__padding '>
       <div>
-        <img src={steam} alt="steam" />
+        <a href='http://store.steampowered.com/app/504230/Celeste/'>
+          <img src={steam} alt="steam" />
+        </a>
       </div>
       <div>
-        <img src={xbox} alt="xbox" />
+        <a href='https://www.microsoft.com/en-ca/store/p/celeste/bwmql2rpwbhb'>
+          <img src={xbox} alt="xbox" />
+        </a>
       </div>
       <div>
-        <img src={ps} alt="ps" />
+        <a href='https://www.playstation.com/en-ca/games/celeste-ps4/'>
+          <img src={ps} alt="ps" />
+        </a>
       </div>
       <div>
-        <img src={epic} alt="epic" />
+        <a href='https://store.epicgames.com/en-US/p/celeste'>
+          <img src={epic} alt="epic" />
+        </a>
       </div>
       <div>
-        <img src={nintendo} alt="nintendo" />
+        <a href='https://www.nintendo.com/store/products/celeste-switch/'>
+          <img src={nintendo} alt="nintendo" />
+        </a>
       </div>
     </div>
   )
